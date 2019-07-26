0a2bb24b10c38bb31ee7325703c7b24040617f6e
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-07-26 15:45:01 +0000

    updated cli, bin and library files and dependencies

--- lib/movie_times.rb
+++ lib/movie_times.rb
@@ -1,6 +1,8 @@
-require "movie_times/version"
+require_relative "./movie_times/version"
 
 module MovieTimes
-  class Error < StandardError; end
+  #class Error < StandardError; end
   # Your code goes here...
 end
+
+require_relative './movie_times/cli'
---
+++ lib/movie_times/cli.rb
@@ -0,0 +1,7 @@
+class MovieTimes::CLI 
+  
+  def call 
+    puts "Coming Soon to a Theatre Near You:"
+  end 
+  
+end 
