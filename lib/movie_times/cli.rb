c2cedbd76fd09e0068d3afbb7a26db8591b8a6db
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-07-26 16:24:34 +0000

    stubbing out the CLI controller

--- lib/movie_times/cli.rb
+++ lib/movie_times/cli.rb
@@ -1,7 +1,22 @@
 class MovieTimes::CLI 
   
   def call 
-    puts "Coming Soon to a Theatre Near You:"
+    list_movies
+    menu
   end 
   
+  def list_movies
+    # here doc
+    puts "Coming Soon to a Theatre Near You:"
+    puts <<-DOC.gsub /^\s*/, ''
+        1. Once Upon a Time in Hollywood - Aug 4th 
+        2. Aladdin - July 31st
+      DOC 
+  end 
+  
+  def menu
+    puts "Enter the number of the movie you'd like to see:"
+    input = gets.strip
+  end 
+   
 end 
