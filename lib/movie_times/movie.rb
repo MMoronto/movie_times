004919cf50a55fe460a72fe4ee09d9093e694a57
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-07-30 18:07:35 +0000

    revised movie.rb to include attribute accessors

--- lib/movie_times/movie.rb
+++ lib/movie_times/movie.rb
@@ -1,15 +1,16 @@
 class MovieTimes::Movie 
+  attr_accessor :title, :duration, :genre, :times_tickets, :releasedate, :availability
   
   def self.release 
     # I should return a bunch of instances of Movie
-    puts "1. Fast & Furious - Hobbs & Shaw - 2019" 
+    puts "Movie 1. Fast & Furious - Hobbs & Shaw - 2019" 
     puts "2. Dora and the Lost City of Gold - 2019" 
     
     movie_1 = self.new 
     movie_1.title = "Fast & Furious - Hobbs & Shaw"
     movie_1.duration = "111 min"
     movie_1.genre = "action thriller"
-    movie_1.times_tickets = "https://meh.com"
+    movie_1.times_tickets = "https://www.imdb.com/title/tt6806448/?ref_=inth_ov_tt"
     movie_1.releasedate = "16 August 2019"
     movie_1.availability = true 
     
@@ -17,11 +18,11 @@ class MovieTimes::Movie
     movie_2.title = "Dora and the Lost City of Gold"
     movie_2.duration = "102 min"
     movie_2.genre = "adventure family"
-    movie_2.times_tickets = "https://meh.com"
+    movie_2.times_tickets = "https://www.imdb.com/title/tt7547410/?ref_=cs_ov_tt"
     movie_2.releasedate = "23 August 2019"
     movie_2.availability = true
     
-    
+    [movie_1, movie_2]
   end 
   
 end 
