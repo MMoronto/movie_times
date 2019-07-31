1f9fc62129f65d10d9b9481be043bbb536c641ad
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-07-31 15:39:44 +0000

    revising movie.rb #self.release method to puts out movie 2.

--- lib/movie_times/movie.rb
+++ lib/movie_times/movie.rb
@@ -4,7 +4,7 @@ class MovieTimes::Movie
   def self.release 
     # I should return a bunch of instances of Movie
     puts "Movie 1. Fast & Furious - Hobbs & Shaw - 2019" 
-    puts "2. Dora and the Lost City of Gold - 2019" 
+    puts "Movie 2. Dora and the Lost City of Gold - 2019" 
     
     movie_1 = self.new 
     movie_1.title = "Fast & Furious - Hobbs & Shaw"
