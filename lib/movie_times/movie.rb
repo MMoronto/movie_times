5e5bf8dafb9553a2b3583f6e72ff8a77736e4d24
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-08-09 16:02:30 +0000

    added nokogiri & pry to dev dependencies in gespec file

--- lib/movie_times/movie.rb
+++ lib/movie_times/movie.rb
@@ -7,23 +7,15 @@ class MovieTimes::Movie
   end 
   
   def self.scrape_movies 
-    movie_1 = self.new 
-    movie_1.title = "Fast & Furious - Hobbs & Shaw"
-    movie_1.duration = "111 min"
-    movie_1.genre = "action thriller"
-    movie_1.times_tickets = "https://www.imdb.com/title/tt6806448/?ref_=inth_ov_tt"
-    movie_1.releasedate = "16 August 2019"
-    movie_1.availability = true 
+    movies = []
     
-    movie_2 = self.new 
-    movie_2.title = "Dora and the Lost City of Gold"
-    movie_2.duration = "102 min"
-    movie_2.genre = "adventure family"
-    movie_2.times_tickets = "https://www.imdb.com/title/tt7547410/?ref_=cs_ov_tt"
-    movie_2.releasedate = "23 August 2019"
-    movie_2.availability = true
+    #Go to IMDB, find movies coming soon 
+    #extract the properties
+    #Instantiate a movie 
     
-    [movie_1, movie_2]
+    #repeat for the next movie 
+    
+    movies 
   end 
   
 end 
