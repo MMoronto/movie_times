1b6153617e6cd83f6b442d27c8840f796146b2c2
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-08-23 16:41:38 +0000

    revise movie_times.rb dependencies

--- lib/movie_times.rb
+++ lib/movie_times.rb
@@ -2,6 +2,9 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
-require_relative "./movie_times/version"
-require_relative './movie_times/movie'
-require_relative './movie_times/cli'
+require_relative "../movie_times/version"
+require_relative '../movie_times/movie'
+require_relative '../movie_times/cli'
+
+module MovieTimes
+end
