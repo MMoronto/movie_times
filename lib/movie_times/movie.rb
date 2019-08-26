449a7913aad16f03c33e90cf9a067fd1431267d1
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-08-26 17:52:36 +0000

    revised web scraper code in movie.rb

--- lib/movie_times/movie.rb
+++ lib/movie_times/movie.rb
@@ -1,35 +1,48 @@
 class MovieTimes::Movie 
-  attr_accessor :title, :availability, :get_tickets 
+  attr_accessor :title, :rel_date, :url, :summary
   
-  def self.release 
+  def initialize(title = nil, url = nil)
+    @title = title
+    @url = url
+  end
+
+  def self.all
+    @@all ||= scrape_coming_soon
+  end
+  
+  def self.find(id)  
     # Scrape IMDB and return movie premiere listings based on that data 
-    self.scrape_movies  
+    self.all[id-1]  
   end 
   
-  def self.scrape_movies 
-    movies = []
-    
-    movies << self.scrape_imdb
-    
-    #Go to IMDB, find movies coming soon 
-    #extract the properties
-    #Instantiate a movie 
-    
-    #repeat for the next movie 
-    
-    movies 
+  def self.find_by_title(title) 
+    self.all.detect do |m|
+      m.name.downcase.strip == name.downcase.strip ||
+      m.name.spli("(").first.strip.downcase == name.downcase.strip
+    end 
   end 
   
-  def self.scrape_imdb
-    doc = Nokogiri::HTML(open("https://www.imdb.com"))
-        #binding.pry 
+  def rel_date
+    @rel_date ||= rel_date_doc.search("p.releaseDate").text.strip
+  end 
+  
+  def summary
+    @summary ||= plot_summary_doc.search("p.plotSummary").text.strip
+  end 
     
-    movie = self.new 
-    movie.title = doc.search(div.title).attr("href").strip
-    movie.availability = true
-    movie.get_tickets = doc.css(div.action).attr("href").strip
-    
-    movie
+  def self.scrape_coming_soon
+    doc = Nokogiri::HTML(open("https://www.imdb.com/coming-soon"))
+        
+    titles = doc.search("h4[itemprop='title'] a[itemprop='url']")
+    titles.collect{|e| new(e.text.strip, "http://imdb.com#{e."href").split("?").first.strip}")}
+  end 
+  
+  def plot_summary_doc
+    @plot_summary_doc ||= Nokogiri::HTML(open("#{self.url}plotsummary"))
+  end 
+  
+  def doc 
+    @doc ||=Nokogiri::HTML(open(self.url))
   end 
   
 end 
